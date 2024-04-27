f6975b9ea6bca78c931833203f53c2a21a651f0f
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-27 12:47:38 -0300

    added some temporary styles to layout and login page

--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,7 +1,43 @@
 import React from "react";
 
 function page() {
-  return <div>page</div>;
+  return (
+    <section className="flex flex-col place-items-center">
+      <span className="text-4xl md:text-7xl mt-4 text-center tracking-tight">
+        Login
+      </span>
+      <div className="border-black bg-slate-900 border shadow-xl rounded-lg mt-16 py-8 px-6 w-1/3 relative">
+        <form>
+          <div className="flex flex-col items-start justify-center gap-8">
+            <div className="flex gap-2">
+              <label htmlFor="email">Email</label>
+              <input
+                type="email"
+                id="email"
+                name="email"
+                className="border-black border rounded-lg"
+              />
+            </div>
+            <div className="flex gap-2">
+              <label htmlFor="password">Password</label>
+              <input
+                type="password"
+                id="password"
+                name="password"
+                className="border-black border rounded-lg"
+              />
+            </div>
+            <button
+              type="submit"
+              className="bg-black text-white rounded-lg p-2"
+            >
+              Login
+            </button>
+          </div>
+        </form>
+      </div>
+    </section>
+  );
 }
 
 export default page;
