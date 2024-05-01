9561dc77ee134b051aba2f3db792bde6e0bbca71
PatrickThompson9 <patrick@aoms-tech.com>
2024-04-30 22:20:16 -0300

    added credentials login

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,12 +1,50 @@
-import NextAuth from "next-auth";
+import NextAuth, { User } from "next-auth";
 import GoogleProvider from "next-auth/providers/google";
 import GitHubProvider from "next-auth/providers/github";
+import Credentials from "next-auth/providers/credentials";
 
 export const authOptions = {
   providers: [
-    GitHubProvider({
-      clientId: process.env.GITHUB_ID ?? "",
-      clientSecret: process.env.GITHUB_SECRET ?? "",
+    Credentials({
+      name: "Credentials",
+      credentials: {
+        username: { label: "Username", type: "text", placeholder: "JohnDoe" },
+        password: {
+          label: "Password",
+          type: "password",
+          placeholder: "Password",
+        },
+      },
+      async authorize(credentials): Promise<User | null> {
+        // TODO: Implement database check instead of hardcoded users
+        const users = [
+          {
+            id: "1",
+            name: "John Doe",
+            username: "JohnDoe",
+            email: "spaghet@yahoo.com",
+            password: "password",
+          },
+          {
+            id: "2",
+            name: "Jane Doe",
+            username: "JaneDoe",
+            email: "meatball@yahoo.com",
+            password: "123",
+          },
+        ];
+
+        const user = credentials
+          ? users.find(
+              (user) =>
+                user.username === credentials.username &&
+                user.password === credentials.password
+            )
+          : null;
+        return user
+          ? { id: user.id, name: user.name, email: user.email }
+          : null;
+      },
     }),
 
     GoogleProvider({
@@ -19,6 +57,11 @@ export const authOptions = {
           response_type: "code",
         },
       },
+    }),
+
+    GitHubProvider({
+      clientId: process.env.GITHUB_ID ?? "",
+      clientSecret: process.env.GITHUB_SECRET ?? "",
     }),
   ],
 };
