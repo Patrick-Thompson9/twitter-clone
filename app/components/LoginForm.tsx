ab611e20de960f7ccfe47caf165739cfec98f7e7
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-01 11:44:41 -0300

    moved client side stuff to LoginForm component and added more styles to form like a gradient behind login fields and added login credentials to login button

---
+++ app/components/LoginForm.tsx
@@ -0,0 +1,92 @@
+"use client";
+import React from "react";
+import ShinyButton from "./ShinyButton";
+import { signIn } from "next-auth/react";
+
+type Provider = {
+  name: string;
+  id: string;
+  signinUrl: string;
+};
+
+interface Props {
+  providers: Provider[];
+}
+
+function LoginForm({ providers }: Props) {
+  const handleSubmit = (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    const form = e.target as HTMLFormElement;
+    const email = form.elements.namedItem("email") as HTMLInputElement;
+    const password = form.elements.namedItem("password") as HTMLInputElement;
+    const username = form.elements.namedItem("username") as HTMLInputElement;
+
+    // TODO: redirect to login if incorrect credentials
+    signIn("credentials", {
+      username: username.value,
+      password: password.value,
+    });
+  };
+
+  return (
+    <form onSubmit={handleSubmit}>
+      <div className="flex flex-col items-start justify-center px-14 -z-20">
+        <label htmlFor="username" className="">
+          Username
+        </label>
+        <div className="relative w-full">
+          <input
+            type="text"
+            id="username"
+            name="username"
+            className="border-black bg-slate-600 border rounded-md my-1 w-full py-1 px-2 focus:outline-none focus:ring-0 focus:border-sky-200 focus:border focus:shadow-sm focus:shadow-sky-200 relative z-10 peer"
+          />
+          <div className="absolute inset-2 opacity-0 bg-sky-200/50 blur-xl filter -z-0 peer-focus:opacity-100 transition-opacity duration-300" />
+        </div>
+        <label htmlFor="password" className="mt-6">
+          Password
+        </label>
+        <div className="relative w-full">
+          <input
+            type="password"
+            id="password"
+            name="password"
+            className="border-black bg-slate-600 border rounded-md my-1 w-full py-1 px-2 focus:outline-none focus:ring-0 focus:border-sky-200 focus:border focus:shadow-sm focus:shadow-sky-200 relative z-10 peer"
+          />
+          <div className="absolute inset-2 opacity-0 bg-sky-200/50 blur-xl filter -z-0 peer-focus:opacity-100 transition-opacity duration-300" />
+        </div>
+        <button type="submit" className="mt-4 w-full">
+          <ShinyButton buttonText="Login" />
+        </button>
+
+        {/* "Or" separator */}
+        <div className="flex items-center justify-center w-full gap-4">
+          <div className="bg-sky-200/75 h-px w-full place-self-center my-10"></div>
+          <span className="font-medium tracking-wide">Or</span>
+          <div className="bg-sky-200/75 h-px w-full place-self-center my-10"></div>
+        </div>
+
+        {/* Sign in With Providers Options */}
+        <div className="flex flex-col items-center justify-center w-full gap-4">
+          <span className="text-lg">Sign in with:</span>
+          <div className="flex flex-col items-center justify-center w-full">
+            {(Object.values(providers) as Provider[]).map(
+              (provider: Provider) => (
+                <button
+                  key={provider.id}
+                  type="submit"
+                  className="mt-4 w-full"
+                  onClick={async () => await signIn(provider.id)}
+                >
+                  <ShinyButton buttonText={provider.name} />
+                </button>
+              )
+            )}
+          </div>
+        </div>
+      </div>
+    </form>
+  );
+}
+
+export default LoginForm;
--- app/login/page.tsx
+++ app/login/page.tsx
@@ -1,35 +1,11 @@
-"use client";
 import React from "react";
-import { getProviders, signIn } from "next-auth/react";
-import ShinyButton from "../components/ShinyButton";
+import LoginForm from "../components/LoginForm";
 import moonSVG from "../assets/moon.svg";
 
-interface Provider {
-  name: string;
-  id: string;
-  signinUrl: string;
-}
-
 async function page() {
-  // const res = await fetch("http://localhost:3000/api/auth/providers");
-  // const providers = await res.json();
-  // console.log(providers);
-  const providers = {
-    github: {
-      id: "github",
-      name: "GitHub",
-      type: "oauth",
-      signinUrl: "http://localhost:3000/api/auth/signin/github",
-      callbackUrl: "http://localhost:3000/api/auth/callback/github",
-    },
-    google: {
-      id: "google",
-      name: "Google",
-      type: "oauth",
-      signinUrl: "http://localhost:3000/api/auth/signin/google",
-      callbackUrl: "http://localhost:3000/api/auth/callback/google",
-    },
-  };
+  const res = await fetch("http://localhost:3000/api/auth/providers");
+  const providers = await res.json();
+  console.log(providers);
 
   return (
     <section className="flex flex-col place-items-center">
@@ -38,60 +14,7 @@ async function page() {
         Login
       </span>
       <div className="border-sky-200/45 bg-gray-900 border shadow-xl rounded-lg mt-16 py-4 px-2 w-2/3 md:w-3/5 lg:py-8 lg:px-6 lg:w-2/5">
-        <form
-          onSubmit={(e) => {
-            e.preventDefault();
-            return false;
-          }}
-        >
-          <div className="flex flex-col items-start justify-center px-14 -z-20">
-            <label htmlFor="email">Email</label>
-            <input
-              type="email"
-              id="email"
-              name="email"
-              className="border-black bg-slate-600 border rounded-md my-1 w-full"
-            />
-            <label htmlFor="password" className="mt-6">
-              Password
-            </label>
-            <input
-              type="password"
-              id="password"
-              name="password"
-              className="border-black bg-slate-600 border rounded-md my-1 pr-10 w-full"
-            />
-            <button type="submit" className="mt-4 w-full">
-              <ShinyButton buttonText="Login" />
-            </button>
-
-            {/* "Or" separator */}
-            <div className="flex items-center justify-center w-full gap-4">
-              <div className="bg-sky-200/75 h-px w-full place-self-center my-10"></div>
-              <span className="font-medium tracking-wide">Or</span>
-              <div className="bg-sky-200/75 h-px w-full place-self-center my-10"></div>
-            </div>
-
-            {/* Sign in With Providers Options */}
-            <div className="flex flex-col items-center justify-center w-full gap-4">
-              <span className="text-lg">Sign in with:</span>
-              <div className="flex flex-col items-center justify-center w-full">
-                {(Object.values(providers) as Provider[]).map(
-                  (provider: Provider) => (
-                    <button
-                      key={provider.id}
-                      type="submit"
-                      className="mt-4 w-full"
-                      onClick={async () => await signIn(provider.id)}
-                    >
-                      <ShinyButton buttonText={provider.name} />
-                    </button>
-                  )
-                )}
-              </div>
-            </div>
-          </div>
-        </form>
+        <LoginForm providers={providers} />
       </div>
     </section>
   );
