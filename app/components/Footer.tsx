f46063bb8ac4fffb9c4fc089e4d09782c163689d
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-01 11:49:32 -0300

    added margin to footer so its further down

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="text-sky-200 font-extralight text-sm">
+    <footer className="text-sky-200 font-extralight text-sm mt-40">
       <div className="w-full bg-sky-200 h-px" />
       <div className="bg-black grid grid-cols-1 gap-1 md:grid-cols-3 min-h-10 justify-items-center content-center py-2">
         <span>Contact</span>
