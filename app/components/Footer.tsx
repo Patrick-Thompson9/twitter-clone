3a62901d36ae43d6e48607fcd61da45af65ec4e9
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-03 12:03:49 -0300

    Added transparent hills to footer that will be in background

--- app/components/Footer.tsx
+++ app/components/Footer.tsx
@@ -2,7 +2,11 @@ import React from "react";
 
 function Footer() {
   return (
-    <footer className="text-sky-200 font-extralight text-sm mt-40">
+    <footer className="relative text-sky-200 font-extralight text-sm mt-40">
+      <img
+        src="hills.svg"
+        className="absolute bottom-0 -z-10 w-full h-auto opacity-25"
+      />
       <div className="w-full bg-sky-200 h-px" />
       <div className="bg-black grid grid-cols-1 gap-1 md:grid-cols-3 min-h-10 justify-items-center content-center py-2">
         <span>Contact</span>
