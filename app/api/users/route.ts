8daef28c7a9666623fea186f76f948048f85df25
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-14 13:00:43 -0300

    added PUT method to route.js users endpoint

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,6 +1,8 @@
 import { NextResponse, NextRequest } from "next/server";
 import { initMongoose } from "../auth/[...nextauth]/lib/mongoose";
 import User from "../../models/User";
+import { getServerSession, unstable_getServerSession } from "next-auth";
+import { authOptions } from "../auth/[...nextauth]/route";
 
 export async function GET(req: NextRequest, res: NextResponse) {
   await initMongoose();
@@ -10,5 +12,11 @@ export async function GET(req: NextRequest, res: NextResponse) {
 }
 
 export async function PUT(req: NextRequest, res: NextResponse) {
+  await initMongoose();
+  const session = await getServerSession(authOptions);
+  const data = await req.json();
+  const username = data.username;
+  console.log(username, session?.user.id);
+  await User.findByIdAndUpdate(session?.user.id, { username });
   return NextResponse.json("Okie Dokie :)");
 }
