3ff3337afa757a5adffbb447f822d5900b90e667
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-14 10:30:09 -0300

    successfully retrieved user

--- app/api/users/route.ts
+++ app/api/users/route.ts
@@ -1,7 +1,11 @@
-import { initMongoose } from "../auth/[...nextauth]/lib/mongoose";
 import { NextResponse, NextRequest } from "next/server";
+import { initMongoose } from "../auth/[...nextauth]/lib/mongoose";
+import User from "../../models/User";
 
 export async function GET(req: NextRequest, res: NextResponse) {
+  await initMongoose();
   const id = req.nextUrl.searchParams.get("id");
-  return NextResponse.json(id);
+  console.log(id);
+  const user = await User.findById(id);
+  return NextResponse.json(user);
 }
