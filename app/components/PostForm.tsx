543d5cd8bd5de694a3d38442d6ed1e7fe9d734ab
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-14 22:17:34 -0300

    added axios, made incomplete PostForm component

---
+++ app/components/PostForm.tsx
@@ -0,0 +1,50 @@
+"use client";
+import { useState } from "react";
+import { GiWolfHowl } from "react-icons/gi";
+import ShinyButton from "./ShinyButton";
+import UserInfo from "@/types/user";
+import axios from "axios";
+
+interface props {
+  userInfo: UserInfo | undefined;
+}
+
+function PostForm({ userInfo }: props) {
+  const [text, setText] = useState("");
+
+  const handleSubmit = async (e: React.FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    console.log(text);
+    const json = await axios.post("/api/posts", { text });
+  };
+
+  return (
+    <div className="flex flex-col rounded-sm relative border-sky-200/75 border p-14 focus-within:border-sky-200 focus ">
+      <div className="flex justify-start gap-2 items-center mb-2">
+        {userInfo?.image ? (
+          <img
+            src={userInfo?.image}
+            className="size-10 rounded-full border border-sky-200"
+            alt="avatar"
+          />
+        ) : (
+          <GiWolfHowl className="size-10 rounded-full border border-sky-200" />
+        )}
+        <span className="font-bold text-xl">What's on your mind?</span>
+      </div>
+      <form className="focus:border-red-500 focus:" onSubmit={handleSubmit}>
+        <textarea
+          className="border-sky-200/75 border resize-none bg-black w-full rounded-md p-4 my-1 focus:outline-none focus:ring-0 focus:border-sky-200 focus:border focus:shadow-md focus:shadow-sky-200/50"
+          placeholder="Write something..."
+          value={text}
+          onChange={(e) => setText(e.target.value)}
+        ></textarea>
+        <button type="submit">
+          <ShinyButton buttonText="Post" classes={"text-center md:max-w-28"} />
+        </button>
+      </form>
+    </div>
+  );
+}
+
+export default PostForm;
--- app/page.tsx
+++ app/page.tsx
@@ -1,9 +1,8 @@
 "use client";
 import useUserInfo from "@/hooks/useUserInfo";
 import UsernameForm from "./components/UsernameForm";
-import ShinyButton from "./components/ShinyButton";
 import Spinner from "./components/Spinner";
-import { GiWolfHowl } from "react-icons/gi";
+import PostForm from "./components/PostForm";
 
 export default function Home() {
   const { userInfo, userInfoStatus } = useUserInfo();
@@ -14,32 +13,9 @@ export default function Home() {
     return <UsernameForm />;
   }
 
-  console.log(userInfo?.image);
   return (
     <section>
-      <div className="flex flex-col rounded-sm relative border-sky-200/75 border p-14 focus-within:border-sky-200 focus ">
-        <div className="flex justify-start gap-2 items-center mb-2">
-          <div className="size-10 rounded-full border border-sky-200">
-            {userInfo?.image ? (
-              <img
-                src={userInfo?.image}
-                className="rounded-full"
-                alt="avatar"
-              />
-            ) : (
-              <GiWolfHowl />
-            )}
-          </div>
-          <span className="font-bold text-xl">What's on your mind?</span>
-        </div>
-        <form className="focus:border-red-500 focus:">
-          <textarea
-            className="border-sky-200/75 border resize-none bg-black w-full rounded-md p-4 my-1 focus:outline-none focus:ring-0 focus:border-sky-200 focus:border focus:shadow-md focus:shadow-sky-200/50"
-            placeholder="Write something..."
-          ></textarea>
-          <ShinyButton buttonText="Post" classes={"text-center md:max-w-28"} />
-        </form>
-      </div>
+      <PostForm userInfo={undefined} />
     </section>
   );
 }
