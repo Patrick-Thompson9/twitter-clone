1dc8659f238b3d3d349c8cc41a03d9003ba71118
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-15 15:07:27 -0300

    increased mt

--- app/not-found.tsx
+++ app/not-found.tsx
@@ -2,7 +2,7 @@ import React from "react";
 
 function NotFoundPage() {
   return (
-    <div className="flex flex-col place-items-center">
+    <div className="flex flex-col place-items-center mt-16">
       <span className="text-sky-100 text-6xl">ERROR</span>
       <div className="flex flex-col justify-center items-center border-sky-200/45 bg-gray-900 border shadow-xl rounded-lg my-12 py-4 px-2 w-2/3 md:w-3/5 lg:py-8 lg:px-6 lg:w-2/5">
         <div className="flex justify-center items-center gap-6 text-sky-100">
