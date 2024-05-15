bc28b9fc00d328fe9a1a9f4348d1eefebbe9d938
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-14 21:44:19 -0300

    made avatar and header on same line

--- app/page.tsx
+++ app/page.tsx
@@ -18,14 +18,20 @@ export default function Home() {
   return (
     <section>
       <div className="flex flex-col rounded-sm relative border-sky-200/75 border p-14 focus-within:border-sky-200 focus ">
-        <div className="size-10 rounded-full border border-sky-200">
-          {userInfo?.image ? (
-            <img src={userInfo?.image} className="rounded-full" alt="avatar" />
-          ) : (
-            <GiWolfHowl />
-          )}
+        <div className="flex justify-start gap-2 items-center mb-2">
+          <div className="size-10 rounded-full border border-sky-200">
+            {userInfo?.image ? (
+              <img
+                src={userInfo?.image}
+                className="rounded-full"
+                alt="avatar"
+              />
+            ) : (
+              <GiWolfHowl />
+            )}
+          </div>
+          <span className="font-bold text-xl">What's on your mind?</span>
         </div>
-        <span className="font-bold">What's on your mind?</span>
         <form className="focus:border-red-500 focus:">
           <textarea
             className="border-sky-200/75 border resize-none bg-black w-full rounded-md p-4 my-1 focus:outline-none focus:ring-0 focus:border-sky-200 focus:border focus:shadow-md focus:shadow-sky-200/50"
