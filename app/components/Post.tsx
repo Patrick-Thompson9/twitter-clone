30e568c97df09050ecd6c58030e9c6533d9ac2eb
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-16 11:20:06 -0300

    Added links to post header and body

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -6,6 +6,7 @@ import PostData from "@/types/post";
 import ReactTimeAgo from "react-time-ago";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
+import Link from "next/link";
 
 TimeAgo.addLocale(en);
 
@@ -20,39 +21,44 @@ interface props {
 }
 
 function Post({ postData }: props) {
+  console.log("PostData", postData);
   return (
     <div className="flex flex-col justify-start place-items-center relative my-3 w-full">
       {/* post card */}
       <div className="flex flex-col justify-start relative bg-slate-950 border-sky-200/75 border p-14 rounded-lg py-4 px-2 w-2/3 md:w-3/5 lg:py-8 lg:px-6 lg:w-2/5 hover:shadow-xl hover:shadow-sky-200/20 transition-shadow duration-300">
         {/* post header */}
-        <div className="flex justify-start gap-2 items-center mb-2">
-          {postData.author.image ? (
-            <img
-              src={postData.author.image}
-              className="size-12 rounded-full border border-sky-200"
-              alt="avatar"
-            />
-          ) : (
-            <GiWolfHowl className="size-14 rounded-full border border-sky-200" />
-          )}
-          <div className="flex justify-between items-start w-full">
-            <div className="flex flex-col justify-start">
-              <span className="text-xl">{postData.author.name}</span>
+        <Link href={"/user?id=" + postData.author.id}>
+          <div className="flex justify-start gap-2 items-center mb-2">
+            {postData.author.image ? (
+              <img
+                src={postData.author.image}
+                referrerPolicy="no-referrer"
+                className="size-12 rounded-full border border-sky-200"
+              />
+            ) : (
+              <GiWolfHowl className="size-14 rounded-full border border-sky-200" />
+            )}
+            <div className="flex justify-between items-start w-full">
+              <div className="flex flex-col justify-start">
+                <span className="text-xl">{postData.author.name}</span>
+                <span className="text-sky-200/50">
+                  {postData.author.username}
+                </span>
+              </div>
               <span className="text-sky-200/50">
-                {postData.author.username}
+                <ReactTimeAgo date={postData.createdAt} />
               </span>
             </div>
-            <span className="text-sky-200/50">
-              <ReactTimeAgo date={postData.createdAt} />
-            </span>
           </div>
-        </div>
+        </Link>
 
         {/* post body */}
-        <div className="border border-sky-200/25 w-full min-h-20 rounded-sm py-1 px-2">
-          {/* post text */}
-          {postData.text}
-        </div>
+        <Link href={`/${postData.author.username}/status/${postData._id}`}>
+          <div className="border border-sky-200/25 w-full min-h-20 rounded-sm py-1 px-2">
+            {/* post text */}
+            {postData.text}
+          </div>
+        </Link>
 
         {/* post widgets */}
         <div className="flex gap-3 mt-2 justify-start items-center">
