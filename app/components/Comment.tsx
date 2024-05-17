6184797177a39b631eb51c3c8b0d505ed3fd2ba5
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-17 11:12:13 -0300

    changed from user to postData and added time ago

--- app/components/Comment.tsx
+++ app/components/Comment.tsx
@@ -1,18 +1,22 @@
-import UserInfo from "@/types/user";
 import React from "react";
 import UserHeader from "./UserHeader";
+import PostData from "@/types/post";
+import ReactTimeAgo from "react-time-ago";
 import TimeAgo from "javascript-time-ago";
+import en from "javascript-time-ago/locale/en";
+
+TimeAgo.addLocale(en);
 
 interface props {
-  User: UserInfo;
+  postData: PostData;
 }
 
-function Comment({ User }: props) {
+function Comment({ postData }: props) {
   return (
     <div className="px-4 py-2 bg-slate-900">
-      <UserHeader User={User} />
+      <UserHeader User={postData.author} />
       <div className="p-2">Comment</div>
-      {/* <TimeAgo date={} /> */}
+      <ReactTimeAgo date={postData.createdAt} className="text-sky-200/50" />
     </div>
   );
 }
