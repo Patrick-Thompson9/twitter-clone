3ecd58e847b9bd4967e8e2e90dd5c0bdbbd830a0
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-17 12:02:38 -0300

    changed UserHeader to be sibling of Link to prevent hydration error

--- app/components/Post.tsx
+++ app/components/Post.tsx
@@ -18,16 +18,10 @@ function Post({ postData }: props) {
   return (
     <div className="flex flex-col justify-center mt-3 card-size">
       {/* post card */}
-      <Link href={`/${postData.author.username}/status/${postData._id}`}>
-        <div className="flex flex-col justify-start bg-slate-950 border-sky-200/75 border p-14 rounded-lg py-4 px-2 lg:py-8 lg:px-6 overflow-auto hover:shadow-xl hover:shadow-sky-200/20 hover:border-sky-200 transition-all duration-300">
+      <div className="flex flex-col justify-start bg-slate-950 border-sky-200/75 border p-14 rounded-lg py-4 pb-2 px-2 lg:py-8 lg:pb-4 lg:px-6 overflow-auto hover:shadow-xl hover:shadow-sky-200/20 hover:border-sky-200 transition-all duration-300">
+        <UserHeader User={postData.author} />
+        <Link href={`/${postData.author.username}/status/${postData._id}`}>
           {/* post header */}
-
-          <div className="flex justify-between gap-2 items-center mb-2 select-auto">
-            <UserHeader User={postData.author} />
-            <span className="text-sky-200/50">
-              <ReactTimeAgo date={postData.createdAt} />
-            </span>
-          </div>
 
           {/* post body */}
           <div className="border border-sky-200/25 w-full min-h-20 rounded-sm py-1 px-2 ">
@@ -36,9 +30,14 @@ function Post({ postData }: props) {
           </div>
 
           {/* post widgets */}
-          <Widgets />
-        </div>
-      </Link>
+          <div className="flex flex-col gap-4">
+            <Widgets />
+            <span className="text-sky-200/50">
+              <ReactTimeAgo date={postData.createdAt} />
+            </span>
+          </div>
+        </Link>
+      </div>
     </div>
   );
 }
