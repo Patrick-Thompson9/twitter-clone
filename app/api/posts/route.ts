c3c83bdfc5eeeda6b74c6cc913e09d4987540088
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-21 12:49:32 -0300

    changed response to an object containing post and likedByMe

--- app/api/posts/route.ts
+++ app/api/posts/route.ts
@@ -2,12 +2,22 @@ import { NextResponse, NextRequest } from "next/server";
 import Post from "../../models/Post";
 import { getServerSession } from "next-auth";
 import { authOptions } from "../auth/[...nextauth]/route";
+import { initMongoose } from "../auth/[...nextauth]/lib/mongoose";
+import Like from "@/app/models/Like";
 
 export async function GET(req: NextRequest, res: NextResponse) {
+  await initMongoose();
+  const session = await getServerSession(authOptions);
   const id = req.nextUrl.searchParams.get("id");
+
   if (id) {
     const post = await Post.findById(id).populate("author");
-    return NextResponse.json(post);
+    const liked = await Like.find({
+      author: session?.user.id,
+      post: post._id,
+    }).exec();
+    const likedByMe = liked.length === 0 ? false : true;
+    return NextResponse.json({ post, likedByMe });
   }
 
   const posts = await Post.find()
@@ -15,7 +25,13 @@ export async function GET(req: NextRequest, res: NextResponse) {
     .sort({ createdAt: -1 })
     .limit(20)
     .exec();
-  return NextResponse.json(posts);
+
+  const postsLikedByMe = await Like.find({
+    author: session?.user.id,
+    post: posts.map((p) => p._id),
+  }).exec();
+  const idsLikedByMe = postsLikedByMe.map((like) => like.post);
+  return NextResponse.json({ posts, idsLikedByMe });
 }
 
 export async function POST(req: NextRequest, res: NextResponse) {
