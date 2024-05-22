1e5901fd48079579969a5f41799ef89266d8ca1f
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-22 14:16:55 -0300

    added post body and link to post on comments

--- app/components/Comment.tsx
+++ app/components/Comment.tsx
@@ -4,6 +4,7 @@ import PostData from "@/types/post";
 import ReactTimeAgo from "react-time-ago";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
+import Link from "next/link";
 
 TimeAgo.addLocale(en);
 
@@ -15,8 +16,13 @@ function Comment({ postData }: props) {
   return (
     <div className="px-4 py-2 bg-slate-900">
       <UserHeader User={postData.author} />
-      <div className="p-2">Comment</div>
-      <ReactTimeAgo date={postData.createdAt} className="text-sky-200/50" />
+      <Link
+        href={`/${postData.author.username}/status/${postData._id}`}
+        className="flex flex-col justify-start items-start p-1 rounded-lg hover:shadow-xl hover:border hover:border-sky-200 hover:shadow-sky-200/20 transition-shadow duration-300"
+      >
+        <div className="p-2">{postData.text}</div>
+        <ReactTimeAgo date={postData.createdAt} className="text-sky-200/50" />
+      </Link>
     </div>
   );
 }
