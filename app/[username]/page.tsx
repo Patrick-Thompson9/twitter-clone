02e551f5e8cf0779dc0dd7ec038460cd91f1bb99
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-22 15:44:56 -0300

    added boilerplate

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -1,7 +1,26 @@
-import React from "react";
+"use client";
+import axios from "axios";
+import { useState, useEffect } from "react";
+import BackButton from "../components/BackButton";
 
 function page({ params }: { params: { username: string } }) {
-  return <section className="mt-20">{params.username}'s Page</section>;
+  const getUser = async () => {
+    const user = await axios.get(`/api/user?username=${params.username}`);
+    return user;
+  };
+
+  //   useEffect(() => {
+  //     getUser();
+  //   }, [params]);
+
+  return (
+    <section className="mt-20">
+      <BackButton />
+      <div className="flex flex-col justify-start items-start">
+        <span>{params.username}</span>
+      </div>
+    </section>
+  );
 }
 
 export default page;
