b9ba3012086033e485f2d738c8b420d47cc9f9b7
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-22 14:34:34 -0300

    added widgets and adjusted classes accoridngly

--- app/components/Comment.tsx
+++ app/components/Comment.tsx
@@ -5,6 +5,7 @@ import ReactTimeAgo from "react-time-ago";
 import TimeAgo from "javascript-time-ago";
 import en from "javascript-time-ago/locale/en";
 import Link from "next/link";
+import Widgets from "./Widgets";
 
 TimeAgo.addLocale(en);
 
@@ -16,13 +17,16 @@ function Comment({ postData }: props) {
   return (
     <div className="px-4 py-2 bg-slate-900">
       <UserHeader User={postData.author} />
-      <Link
-        href={`/${postData.author.username}/status/${postData._id}`}
-        className="flex flex-col justify-start items-start p-1 rounded-lg hover:shadow-xl hover:border hover:border-sky-200 hover:shadow-sky-200/20 transition-shadow duration-300"
-      >
-        <div className="p-2">{postData.text}</div>
-        <ReactTimeAgo date={postData.createdAt} className="text-sky-200/50" />
-      </Link>
+      <div className="flex justify-between items-center">
+        <Link
+          href={`/${postData.author.username}/status/${postData._id}`}
+          className="flex grow flex-col justify-start items-start rounded-lg hover:shadow-xl hover:border hover:border-sky-200 hover:shadow-sky-200/20 transition-shadow duration-300 p-2 mr-2"
+        >
+          <span>{postData.text}</span>
+          <ReactTimeAgo date={postData.createdAt} className="text-sky-200/50" />
+        </Link>
+        <Widgets postData={postData} />
+      </div>
     </div>
   );
 }
