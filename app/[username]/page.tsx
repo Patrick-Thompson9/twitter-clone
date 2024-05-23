1075e402a36bf1c532b38e2488c7d1b21206e1c4
PatrickThompson9 <patrick@aoms-tech.com>
2024-05-22 21:09:34 -0300

    added loading state variable to prevent early push

--- app/[username]/page.tsx
+++ app/[username]/page.tsx
@@ -8,20 +8,23 @@ import { useRouter } from "next/navigation";
 function page({ params }: { params: { username: string } }) {
   const router = useRouter();
   const [profile, setProfile] = useState<UserInfo>();
+  const [isLoading, setIsLoading] = useState(true);
+
   const getUser = async () => {
     const user = await axios
       .get(`/api/users?username=${params.username}`)
-      .then((res) => setProfile(res.data));
-    return user;
+      .then((res) => {
+        setProfile(res.data);
+        setIsLoading(false);
+      });
   };
 
   useEffect(() => {
     getUser();
   }, [params]);
 
-  if (!profile) {
+  if (!profile && !isLoading) {
     router.push("/error/404");
-    return;
   }
 
   return (
@@ -31,15 +34,15 @@ function page({ params }: { params: { username: string } }) {
         <div className="flex flex-col justify-center items-start">
           <div className="relative mb-2 w-full">
             {/* Timeline Image */}
-            <img src={profile.image} className="w-full h-20 md:h-40" />
+            <img src={profile?.image} className="w-full h-20 md:h-40" />
             <img
-              src={profile.image}
+              src={profile?.image}
               className="absolute bottom-0 size-20 rounded-full border border-sky-200 box"
               style={{ boxShadow: "0 0 0 5px #0f172a" }}
             />
           </div>
-          <span className="text-5xl">{profile.name}</span>
-          <span className="text-sky-200/50 text-2xl">{profile.username}</span>
+          <span className="text-5xl">{profile?.name}</span>
+          <span className="text-sky-200/50 text-2xl">{profile?.username}</span>
         </div>
       </div>
     </section>
