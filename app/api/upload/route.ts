b0ab211aea8415187a387b83e4db4f41d33074d7
PatrickThompson9 <patrick@aoms-tech.com>
2025-08-03 18:37:35 +0100

    added comment

--- app/api/upload/route.ts
+++ app/api/upload/route.ts
@@ -2,4 +2,7 @@ import { NextRequest, NextResponse } from "next/server";
 
 export async function POST(req: NextRequest, res: NextResponse) {
   return NextResponse.json("Upload :)");
+  {
+    /* THIS IS WHERE I TOOK A BREAK */
+  }
 }
